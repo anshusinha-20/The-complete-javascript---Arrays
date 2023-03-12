feb789875d9805976468725bcd06b9dcce37f525
Anshu Sinha <anshujuly2@gmail.com>
2023-03-12 12:06:02 +0530

    Add 'coding challenge 3' codes

--- script.js
+++ script.js
@@ -404,3 +404,21 @@ displaySummary(account1.movements);
 //   .reduce((acc, mov) => acc + mov, 0);
 
 // console.log(totalDepositsInUsd);
+
+/////
+
+// 156. coding challenge #3
+// Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time as an arrow function, and using chaining!
+// Test data:
+// § Data1:[5,2,4,1,15,8,3] § Data2:[16,6,10,5,6,1,4]
+// GOOD LUCK 😀
+
+// const calcAverageHumanAge = (ages) =>
+//   ages
+//     .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter((age) => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+// const dogAges = [5, 2, 4, 1, 15, 8, 3];
+
+// console.log(`Average age: ${calcAverageHumanAge(dogAges)}`);
